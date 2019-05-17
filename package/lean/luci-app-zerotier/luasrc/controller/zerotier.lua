73401c9de2a9264a3d6ccb43778a81e506270f08
greyren <terrys@qq.com>
2019-05-17 16:51:28 +0800

    Revert "luci app zerotier: path bug fix"
    This reverts commit 2915c44a11ca0ee40b51ff5d9c18a0da1951e170.

--- package/lean/luci-app-zerotier/luasrc/controller/zerotier.lua
+++ package/lean/luci-app-zerotier/luasrc/controller/zerotier.lua
@@ -6,13 +6,8 @@ return
 end
 
 entry({"admin","vpn"}, firstchild(), "VPN", 45).dependent = false
-
-entry({"admin", "vpn", "zerotier"},firstchild(), _("ZeroTier")).dependent = false
-
-entry({"admin", "vpn", "zerotier", "general"},cbi("zerotier/settings"), _("Base Setting"), 1)
-entry({"admin", "vpn", "zerotier", "log"},form("zerotier/info"), _("Interface Info"), 2)
-
-entry({"admin","vpn","zerotier","status"},call("act_status"))
+entry({"admin","vpn","zerotier"},cbi("zerotier"),_("ZeroTier"),90).dependent=true
+entry({"admin","vpn","zerotier","status"},call("act_status")).leaf=true
 end
 
 function act_status()
